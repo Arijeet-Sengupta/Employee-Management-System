cfa89b3dd15883e3475e137754ec0b8e62e67d56
UTKARSHA GHAGARE <utkarshaghagare03@gmail.com>
2023-07-19 16:47:02 +0530

    Salary class added

---
+++ src/com/aei/employeemanagement/Location.java
@@ -0,0 +1,11 @@
+package com.aei.employeemanagement;
+import java.util.*;
+class Location extends Employee{
+
+    public Location(int empId, String empName, String gender, int salary, String designation, String location) {
+        super(empId, empName, gender, salary, designation, location);
+    }
+    List<Employee> employee = new ArrayList<Employee>() {
+    }
+
+}

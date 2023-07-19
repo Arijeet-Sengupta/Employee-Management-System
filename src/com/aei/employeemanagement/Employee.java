71414c49e263ad94c0b86599675194c4f463a3a4
ArijeetSengupta <134497545+Arijeet-Sengupta@users.noreply.github.com>
2023-07-19 14:41:02 +0530

    Add files via upload

---
+++ src/com/aei/employeemanagement/Employee.java
@@ -0,0 +1,10 @@
+package com.aei.employeemanagement;
+
+public class Employee {
+    int employeeId;
+    String employeeName;
+    String gender;
+    int salary;
+    String designation;
+    String location;
+}

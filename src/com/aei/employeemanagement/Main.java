e70cbae433ce7c0d51484bc58e0680995d849b17
Arijeet-Sengupta <arijeetsengupta0303@gmail.com>
2023-07-20 17:44:12 +0530

    Added Tax calculator according to tax slabs

--- src/com/aei/employeemanagement/Main.java
+++ src/com/aei/employeemanagement/Main.java
@@ -1,8 +1,11 @@
 package com.aei.employeemanagement;
 
+
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-            }
+        TaxCalculation.taxCalculation();
+    }
 }

682d5386b9f185d27d742184055350cde6782aac
karatos001 <yaskadam399@gmail.com>
2023-07-20 13:27:06 +0530

    implementd gender ratio

--- src/com/aei/employeemanagement/Employee.java
+++ src/com/aei/employeemanagement/Employee.java
@@ -8,13 +8,12 @@ public class Employee {
     String designation;
     String location;
 
-    public Employee(int empId,String empName,String gender, int salary, String designation, String location){
-        this.empId = empId;
-        this.empName =empName;
-        this.gender = gender;
-        this.salary = salary;
-        this.designation = designation;
-        this.location = location;
-
+    public Employee(int empId, String empName, String gender, int salary, String designation, String location) {
+        this.empId=empId;
+        this.empName=empName;
+        this.gender=gender;
+        this.salary=salary;
+        this.designation=designation;
+        this.location=location;
     }
 }
